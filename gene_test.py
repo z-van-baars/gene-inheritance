fbfadca84367cc4afa66f37e334bbf00486323d0
Zac <zvanbaars@gmail.com>
2016-01-03 19:08:26 -0800

    idk what's happening

--- gene_test.py
+++ gene_test.py
@@ -1,3 +1,36 @@
+import random
 
 
-def 
+def plus1(x):
+    return (x + 1)
+
+
+def plus5(x):
+    return (x + 5)
+
+
+def minus1(x):
+    return (x - 1)
+
+
+def minus5(x):
+    return (x - 5)
+
+basepairs = {'A': plus1(1), 'B': plus5(1), 'C': minus1(1), 'D': minus5(1)}
+bases = ['A', 'B', 'C', 'D']
+
+
+def build_strand(l):
+    strand = []
+    for x in range(l):
+        choice = random.choice(bases)
+        strand.append(choice)
+    return strand
+
+strand1 = build_strand(20)
+strand2 = build_strand(200)
+
+print strand1[4]
+input("?")
+
+print "none"
